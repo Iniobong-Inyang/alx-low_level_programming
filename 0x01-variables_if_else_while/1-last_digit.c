b2ef41a89f6eefd53739a64d44b92f8fcce77221
Iniobong-Inyang <iniobonginyang619@gmail.com>
2022-09-09 12:34:10 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,21 +1,16 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
 *main - Determines either greater than 5, is less than 6, or is 0
 *Return: Always 0 (Success)
 */
-
 int main(void)
-
 {
 
 						int final;
 
-
 						int n;
-
 
 						srand(time(0));
 						n = rand() - RAND_MAX / 2;
