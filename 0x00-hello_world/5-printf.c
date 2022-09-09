e054c60000622fabeaa5a4d5d7c34718c47f4976
Iniobong-Inyang <iniobonginyang619@gmail.com>
2022-09-08 18:13:40 -0700

    5-printf.fg

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
-#include <stdio.h>
+#include <stdio.h> 
 
-/**
- * main -prints the size of various types
- * Return: 0 if exited properly, non-zero otherwise
- */
-int main(void)
-{
-	printf("with proper grammer, but the outcome is a piece of art,\n");
-	return (0);
-}
+
+ /** 
+  * main - Prints a text 
+  * Return: Always 0 (Success)* 
+  */ 
+ int main(void) 
+{ 
+ printf("with proper grammar, but the outcome is a piece of art,\n"); 
+return (0);
+ }
