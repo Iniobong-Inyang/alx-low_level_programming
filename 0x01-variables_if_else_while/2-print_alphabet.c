3738797596e6708f85fd8eeb86d8678185c9d655
Iniobong-Inyang <iniobonginyang619@gmail.com>
2022-09-09 13:12:55 -0700

    plrasre

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,17 +1,17 @@
-#include <stdio.h>
-
+#include <stdio.h>`
 /**
 *main - me just wan do magic here i no send anybody
 *Return: always 0 (success)
 */
 int main(void)
 {
-char henry="a";
-while (henry <= z)
-{
-putchar("%c ",henry);
-henry++;
-}
-putchar("\n");
-return (0);
+		char henry="a";
+		while (henry <= z)
+		{
+		putchar("%c ",henry);
+		henry++;
+		}
+		putchar("\n");
+		return (0);
+
 }
