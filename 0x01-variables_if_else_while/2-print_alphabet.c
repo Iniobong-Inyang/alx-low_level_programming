af4957e965d335c51a3d174ad7ede22f055889cf
Iniobong-Inyang <iniobonginyang619@gmail.com>
2022-09-09 12:58:34 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/** 
+*main - me just wan do magic here i no send anybody
+*Return: always 0 (success)
+*/
+int main(void)
+{
+char Ini="a";
+while (Ini <= z)
+{
+putchar("%c ",Ini);
+Ini++;
+}
+putchar("\n");
+return (0);
+}
