8cdebf311cdf59ed8c65b2d07c7f5929be666ad1
Iniobong-Inyang <iniobonginyang619@gmail.com>
2022-09-10 01:59:44 -0700

    abeg

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,16 +3,15 @@
 *main - me just wan do magic here i no send anybody
 *Return: always 0 (success) 
 */
-int main(void)
-{
 
-char henry = 'a';
-while(henry <= 'z')
-{
-
-putchar(henry);
-henry++;
-}
-putchar(10);
-return (0);
-}
+	int main(void)
+	{
+	char henry = 'a';
+	while (henry <= 'z')
+	{
+	putchar(henry);
+	henry++;
+	}
+	putchar(10);
+	return (0);
+	}
