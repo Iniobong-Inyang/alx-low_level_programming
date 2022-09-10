1cab8c847885e1b18be490ee695458b2f1f7a64e
Iniobong-Inyang <iniobonginyang619@gmail.com>
2022-09-10 02:20:43 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+*main - put am for betty body wotowoto
+*Return: Always 0 (success)*
+*/
+int main(void)
+{
+char henry = 'a';
+char ini = 'A';
+for (henry = 'a' ; henry <= 'z'; henry++)
+{
+putchar(henry);
+}
+for (ini = 'A' ; ini <= 'Z'; ini++)
+{
+putchar(ini);
+}
+putchar('\n');
+return (0);
+}
