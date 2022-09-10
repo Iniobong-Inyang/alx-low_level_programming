74ba285b7df181433b36b86d9aadd22fb94fc3e0
Iniobong-Inyang <iniobonginyang619@gmail.com>
2022-09-10 02:06:10 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
 *main - me just wan do magic here i no send anybody
-*Return: always 0 (success) 
+*Return:always 0 (success) 
 */
 int main(void)
 {
 char henry = 'a';
-while (henry<='z')
+while (henry <= 'z')
 {
 putchar(henry);
 henry++;
