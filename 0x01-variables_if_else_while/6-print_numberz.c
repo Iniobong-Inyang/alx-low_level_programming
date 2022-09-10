15b0be085f26a1da6142270b5b79930c14d3f39b
Iniobong-Inyang <iniobonginyang619@gmail.com>
2022-09-10 02:41:56 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*main - count one, two and three
+*Return: Always 0 (success)
+*/
+int main(void)
+{
+int m;
+for (m = 48 ; m < 58 ; m++)
+{
+putchar(m);
+}
+putchar('\n');
+return (0);
+}
