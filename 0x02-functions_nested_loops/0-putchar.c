928f4acf79e098be79046d8b77fe3fb8a7be3d5b
Iniobong-Inyang <iniobonginyang619@gmail.com>
2022-09-13 06:32:08 -0700

    Task one

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+*main - Prints "iniobong" followed by a new line.
+*Return: Always 0.
+*/
+int main(void)
+{
+putchar('i');
+putchar('n');
+putchar('i');
+putchar('o');
+putchar('b');
+putchar('o');
+putchar('n');
+putchar('g');
+putchar('\n');
+return (0);
+}
